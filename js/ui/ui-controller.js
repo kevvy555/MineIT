1da10e924449def07f786dcef8b5f507516d3379
kevvy555 <kevvy555@googlemail.com>
2026-08-19 06:46:37 +0100

    Add map filters, sortable collection and detailed help

--- js/ui/ui-controller.js
+++ js/ui/ui-controller.js
@@ -3,6 +3,7 @@ import { ResourceUIMixin } from "./resource-ui.js?v=5.0.0";
 import { ColonyTechUIMixin } from "./colony-tech-ui.js?v=5.0.0";
 import { ContractUIMixin } from "./contract-ui.js?v=5.0.0";
 import { PortfolioUIMixin } from "./portfolio-ui.js?v=5.0.0";
+import { UIEnhancementsMixin } from "./ui-enhancements.js?v=5.2.0";
 
 export class UIController {
   constructor({state,repo,resources,inventory,collection,colony,portfolio,sites,technology,survey,contracts,world,icons,diagnostics,onHardReset,onNewContract,onSwitchColony,onRemoveColony,onMakeLiability,onRelocateColony,onRecalculate}){
@@ -22,4 +23,4 @@ export class UIController {
   stockFor(tile){return this.inventory.amountFor(this.state,tile.type,tile.resourceId);}
 }
 function mix(target,source){const descriptors=Object.getOwnPropertyDescriptors(source.prototype);delete descriptors.constructor;Object.defineProperties(target.prototype,descriptors);}
-mix(UIController,ResourceUIMixin);mix(UIController,ColonyTechUIMixin);mix(UIController,ContractUIMixin);mix(UIController,PortfolioUIMixin);
+mix(UIController,ResourceUIMixin);mix(UIController,ColonyTechUIMixin);mix(UIController,ContractUIMixin);mix(UIController,PortfolioUIMixin);mix(UIController,UIEnhancementsMixin);
