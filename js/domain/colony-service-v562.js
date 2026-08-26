6426d02d84c7ff6df526cdd011fd811dfb5a1461
kevvy555 <kevvy555@googlemail.com>
2026-08-26 10:51:00 +0100

    Collapse legacy colony wrapper v562

--- js/domain/colony-service-v562.js
+++ js/domain/colony-service-v562.js
@@ -1,10 +1,1 @@
-import { ColonyService as LegacyColonyService } from "./colony-service.js?v=5.5.5&legacy=1";
-import { supportsOverdrive,workforceMultiplier,isAccidentShutdown } from "./extraction-overdrive.js?v=5.6.2";
-
-export class ColonyService extends LegacyColonyService{
-  siteWorkforce(state,tile,levelOverride=null,modeOverride=null){
-    const base=super.siteWorkforce(state,tile,levelOverride);
-    return supportsOverdrive(tile)?Math.max(1,Math.ceil(base*workforceMultiplier(tile,modeOverride))):base;
-  }
-  workforceSites(state){return super.workforceSites(state).filter(tile=>!isAccidentShutdown(tile));}
-}
+export * from "./colony-service.js";
