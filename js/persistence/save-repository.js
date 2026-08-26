f946b349802e5ccace6e9551eb13d35c00122feb
kevvy555 <kevvy555@googlemail.com>
2026-08-26 15:20:40 +0100

    Remove query import from save repository

--- js/persistence/save-repository.js
+++ js/persistence/save-repository.js
@@ -1,4 +1,4 @@
-import { CONFIG } from "../core/config.js?v=5.5.5";
+import { CONFIG } from "../core/config.js";
 
 export class SaveRepository {
   constructor(diagnostics){this.diagnostics=diagnostics;this.beforeSave=null;}
